a30e177ea3233c46be31d3e830fc560f999b2355
1moosey1 <jgonzalez498@gmail.com>
2015-06-01 21:35:15 -0700

    Inital Commit

---
+++ android/src/com/qleek/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.qleek.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.qleek.Qleek;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Qleek(), config);
+	}
+}
