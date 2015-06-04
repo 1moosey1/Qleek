042e7a76858b7656e82981230bcd1738b0e1ddc1
1moosey1 <jgonzalez498@gmail.com>
2015-06-04 15:46:21 -0700

    Added test ui resources as well as screen and widget packages. HUD testing.

--- core/src/com/qleek/Qleek.java
+++ core/src/com/qleek/Qleek.java
@@ -1,28 +1,26 @@
 package com.qleek;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.qleek.screens.GameScreen;
 
-public class Qleek extends ApplicationAdapter {
+public class Qleek extends Game {
 	
-	SpriteBatch batch;
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		
 		batch = new SpriteBatch();
+		setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		batch.begin();
-		batch.end();
+		super.render();
+	}
+	
+	public void dispose() {	
+		batch.dispose();
 	}
 }
---
+++ core/src/com/qleek/screens/GameScreen.java
@@ -0,0 +1,101 @@
+package com.qleek.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.*;
+import com.qleek.Qleek;
+
+public class GameScreen implements Screen {
+	
+	private final Qleek qleek;
+	private Stage hud;
+	private Table hudLayout;
+	
+	public GameScreen(Qleek game) {
+		
+		qleek = game;
+		
+		hud = new Stage(new ScreenViewport(), qleek.batch);
+		
+		//InputMultiplexer ip = new InputMultiplexer();
+		//ip.addProcessor(hud);
+		Gdx.input.setInputProcessor(hud);
+		
+		hudLayout = new Table();
+		hudLayout.setDebug(true);
+		hudLayout.setFillParent(true);
+		hud.addActor(hudLayout);
+		
+		Skin uiskin = new Skin(Gdx.files.internal("uiskin.json"));		
+		Label testLabel = new Label("Label Test", uiskin);
+		TextButton testButton = new TextButton("Button Test", uiskin);
+		
+		
+		testButton.addListener(new InputListener() {
+			
+			@Override
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+		        System.out.println("down");
+		        return true;
+		    }
+		});
+		
+		
+		hudLayout.add(testLabel).width(100).height(100);
+		hudLayout.row();
+		hudLayout.add(testButton).width(100).height(100);
+	}
+	
+	public void create() {}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		hud.act(delta);
+		hud.draw();
+		
+		//qleek.batch.begin();
+		//qleek.batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+		hud.dispose();
+	}
+}
